929dd13f391ee63f4dc2754c6b7205c2c87889a7
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-07-29 20:26:48 +0200

    FEAT echo cmd

--- main.c
+++ main.c
@@ -13,13 +13,24 @@ int main() {
     char input[100];
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
+    // Copy input to parse commands and arguments
+    char _input[100], param[100];
+    strcpy(_input, input);
+
+    char *command = strtok(_input, " ");
+    int n = (int) strlen(command) + 1;
+
+    strncpy(param, input + n, strlen(input));
 
     // strcmp returns 0 if both strings are equal, 0 evaluates to false inside the "if".
-    if (!strcmp(input, "exit")) {
+    if (!strcmp(command, "exit")) {
       exit(0);
     }
-
-    // If the command doesn't exist print an error
-    printf("rsh: %s: command not found...\n", input);
+    if (!strcmp(command, "echo")) {
+      printf("%s\n", param);
+      continue;
+    }
+      // If the command doesn't exist print an error
+      printf("rsh: %s: command not found...\n", input);
   }
 }

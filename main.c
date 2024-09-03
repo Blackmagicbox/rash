ea6a3894eabdccd480f279659a114d21cab1a66c
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-09-03 13:38:40 +0200

    Feat line read

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
+//#include <stdio.h>
 #include "main.h"
 
+
 int main(int argc, char *argv[]){
+
+  while (1) {
     (void)argc, (void)argv;
-    write(STDOUT_FILENO, "€ ", 8);
+    char *buf = NULL;
+    size_t count = 0;
+    ssize_t nread;
+
+    write(STDOUT_FILENO, "€ ", 5);
+    nread = getline(&buf, &count, stdin);
+
+    if (nread == -1) {
+      perror("Exiting shell");
+      exit(1);
+    }
+
+    printf("%s", buf);
+    free(buf);
+
     return (0);
+  }
 }

87f4244f53835212db6571eed6dab4d0c076adbe
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-09-06 12:17:10 +0200

    Reformat code to 4 spaces tab

--- main.c
+++ main.c
@@ -2,25 +2,25 @@
 #include "main.h"
 
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
-  while (1) {
-    (void)argc, (void)argv;
-    char *buf = NULL;
-    size_t count = 0;
-    ssize_t nread;
+    while (1) {
+        (void) argc, (void) argv;
+        char *buf = NULL;
+        size_t count = 0;
+        ssize_t nread;
 
-    write(STDOUT_FILENO, "€ ", 5);
-    nread = getline(&buf, &count, stdin);
+        write(STDOUT_FILENO, "€ ", 5);
+        nread = getline(&buf, &count, stdin);
 
-    if (nread == -1) {
-      perror("Exiting shell");
-      exit(1);
+        if (nread == -1) {
+            perror("Exiting shell");
+            exit(1);
+        }
+
+        printf("%s", buf);
+        free(buf);
+
+        return (0);
     }
-
-    printf("%s", buf);
-    free(buf);
-
-    return (0);
-  }
 }
